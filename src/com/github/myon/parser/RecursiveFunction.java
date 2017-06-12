7dc60817df857eca7a2b8e193f6ca4bf301d2d26
0xMyon <0xMyon@gmail.com>
2017-06-13 00:18:48 +0200

    moved parser to extra project

--- src/com/github/myon/parser/RecursiveFunction.java
+++
@@ -1,7 +0,0 @@
-package com.github.myon.parser;
-
-import java.util.function.Function;
-
-public interface RecursiveFunction<F extends Function<?, ?>> extends Function<RecursiveFunction<F>, F> {
-
-}
